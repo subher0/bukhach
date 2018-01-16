7495d91c17cd28e7c43e9f7e574e54ee10c3ddde
subher0 <subher0@mail.ru>
2018-01-16 11:54:07 +0300

    Moved pages to templates

--- gobuhatwithme/site/urls.py
+++ gobuhatwithme/site/urls.py
@@ -4,4 +4,6 @@ from gobuhatwithme.site.views import main_view
 
 urlpatterns = [
     path(r'', main_view.index_view),
+    path(r'about', main_view.about_us_view),
+    path(r'match', main_view.match_view),
 ]
