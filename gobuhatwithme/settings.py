72b90b6272e690151f3a5ae4715b796d95862816
subher0 <subher0@mail.ru>
2018-01-22 01:21:29 +0300

    VCS integration check BKCH-25

--- gobuhatwithme/settings.py
+++ gobuhatwithme/settings.py
@@ -115,9 +115,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/static")
