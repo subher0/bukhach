45fd08f2454c9db03f0c75e341eea4138b323f56
subher0 <subher0@mail.ru>
2018-01-30 23:53:40 +0300

    BKCH-23 Navbar avatar fail with anonymous user fix

--- bukhach/context_processors.py
+++ bukhach/context_processors.py
@@ -2,6 +2,8 @@ from bukhach.models.profile_models import Profile
 
 
 def navbar_avatar(request):
+    if request.user.is_anonymous:
+        return {}
     profile = Profile.objects.filter(user=request.user).first()
     context = {
         'profile_preloaded': profile
