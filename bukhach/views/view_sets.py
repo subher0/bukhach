6a298f6677e4c797dcb186787eff3b030679b3e1
zoola969 <zoola969@yandex.ru>
2018-02-12 22:18:47 +0300

    BKCH-55 some gayshit

--- bukhach/views/view_sets.py
+++ bukhach/views/view_sets.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
-from bukhach.serializers import UserSerializer, GroupSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from bukhach.serializers import UserSerializer, GroupSerializer, ProfileSerializer
 from django.contrib.auth.models import User, Group
+from bukhach.models.profile_models import Profile
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +16,14 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class ProfileView(GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProfileSerializer
+
+    def get(self, request):
+        profile = Profile.objects.filter(user = request.user).first()
+        response = Response({'first_name': profile.user.first_name})
+        response['Access-Control-Allow-Origin'] = 'http://176.112.233.43:3000'
+        return response
