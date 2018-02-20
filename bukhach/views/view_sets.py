8c95ac1d57749533e39d52b3fb40ca278b0c15b1
zoola969 <zoola969@yandex.ru>
2018-02-20 19:03:57 +0300

    BKCH-57 1 method added

--- bukhach/views/view_sets.py
+++ bukhach/views/view_sets.py
@@ -43,6 +43,21 @@ class ProfileSearchView(viewsets.ViewSet):
     permission_classes = (IsAuthenticated,)
 
     def get(self,request):
+
+        def info_append(users, content):
+            for user in users:
+                element = {'first_name': user.first_name,
+                           'last_name': user.last_name,
+                           'username': user.username,
+                           'email': user.email,
+                           'info': user.profile.info,
+                           'tel_num': user.profile.tel_num,
+                           'rating': user.profile.rating,
+                           'avatar': str(user.profile.avatar)
+                           }
+                content.append(element)
+            return content
+
         content = []
         name = request.GET.get('name')
         if name == '':
@@ -52,33 +67,11 @@ class ProfileSearchView(viewsets.ViewSet):
             if len(words) == 1:
                 name = words[0]
                 users = User.objects.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
-                for user in users:
-                    element = {'first_name': user.first_name,
-                               'last_name': user.last_name,
-                               'username': user.username,
-                               'email': user.email,
-                               'info': user.profile.info,
-                               'tel_num': user.profile.tel_num,
-                               'rating': user.profile.rating,
-                               'avatar': str(user.profile.avatar)
-                               }
-                    content.append(element)
-                response = Response(content)
+                response = Response(info_append(users, content))
                 return response
             elif len(words) == 2:
                 f_name = words[0]
                 l_name = words[1]
                 users = User.objects.filter(first_name__icontains=f_name, last_name__icontains=l_name)
-                for user in users:
-                    element = {'first_name': user.first_name,
-                               'last_name': user.last_name,
-                               'username': user.username,
-                               'email': user.email,
-                               'info': user.profile.info,
-                               'tel_num': user.profile.tel_num,
-                               'rating': user.profile.rating,
-                               'avatar': str(user.profile.avatar)
-                               }
-                    content.append(element)
-                response = Response(content)
+                response = Response(info_append(users, content))
                 return response
